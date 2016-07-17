d443b62f9ae2443f455364a4618f8f24fd765930
Will Dawson <mail@willdawson.me>
2016-07-17 23:55:41 +1000

    Convert Coffeescript to ES6.

--- spec/util-spec.js
+++ spec/util-spec.js
@@ -1,7 +1,9 @@
-util = require '../lib/util'
+'use babel'
 
-describe "filterProjects", ->
-  rows = [
+import * as util from '../lib/util'
+
+describe('filterProjects', () => {
+  let rows = [
     { title: 1, paths: ['/Projects/', '/www/side'] },
     { title: 2, paths: ['/Projects/', '/next'] },
     { title: 3, paths: ['/tmp/Projects/'] },
@@ -11,48 +13,53 @@ describe "filterProjects", ->
     { title: 7, paths: ['/some/folder/here'] },
     { title: 8, paths: ['/Workspace/b', '/Workspace/c'] },
     { title: 9, paths: ['/Workspace.bak/a'] },
-    { title: 10, paths: ['/Workspace.bak/.git/secret'] },
+    { title: 10, paths: ['/Workspace.bak/.git/secret'] }
   ]
 
-  beforeEach ->
-    waitsForPromise ->
-      atom.packages.activatePackage('project-plus')
+  beforeEach(() =>
+    waitsForPromise(() => atom.packages.activatePackage('project-plus'))
+  )
 
-  afterEach ->
-    atom.config.set('project-plus.projectHome', '')
+  afterEach(() => atom.config.set('project-plus.projectHome', ''))
 
-  it "should return no projects when none match the whitelist", ->
+  it('should return no projects when none match the whitelist', () => {
     atom.config.set('project-plus.projectHome', 'exclude_all')
     expect(util.filterProjects(rows)).toEqual([])
+  })
 
-  it "should inlude only projects that match the whitelist", ->
+  it('should inlude only projects that match the whitelist', () => {
     atom.config.set('project-plus.projectHome', '/some/folder/here')
     expect(util.filterProjects(rows).length).toEqual(1)
+  })
 
-  it "should include projects that are within a whitelisted folder", ->
+  it('should include projects that are within a whitelisted folder', () => {
     atom.config.set('project-plus.projectHome', '/Projects')
     expect(util.filterProjects(rows).length).toEqual(4)
 
     atom.config.set('project-plus.projectHome', '/Workspace')
     expect(util.filterProjects(rows).length).toEqual(1)
+  })
 
-  it "should always include projects that have been saved", ->
-    items = rows.slice(0)
+  it('should always include projects that have been saved', () => {
+    const items = rows.slice(0)
     items.push({ title: 11, paths: ['outside_of_home'], provider: 'file' })
 
     atom.config.set('project-plus.projectHome', '/Projects')
     expect(util.filterProjects(items).length).toEqual(5)
+  })
 
-  it "should filter out projects that don't make sense", ->
-    items = [
-      # No paths array
+  it("should filter out projects that don't make sense", () => {
+    const items = [
+      // No paths array
       {},
-      # No paths
+      // No paths
       {paths: []},
-      # 1 path but it contains something weird
+      // 1 path but it contains something weird
       {paths: [2]},
-      # Something okay here
-      {paths: ["/work"]}
+      // Something okay here
+      {paths: ['/work']}
     ]
 
     expect(util.filterProjects(items).length).toEqual(1)
+  })
+})
